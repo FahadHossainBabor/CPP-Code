9da6297704275c22ef9b4a8fe0906adfae4e5dbb
Fahad Hossain Babor <fahadhossainbabor.cse.ruet@gmail.com>
2024-04-01 05:41:04 +0600

    Added void

---
+++ Basics/void.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+//void means the function doesn't have any return value 
+void printHi()
+{
+    std::cout<<"Hi"<<'\n';
+}
+
+int main() 
+{
+    printHi();
+
+    return 0;
+}
